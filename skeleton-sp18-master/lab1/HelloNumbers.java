d0eeb4695fc6011340d5e26691b9d2e595d80d8d
ScofieldaCreep <tju_scofield@outlook.com>
2021-12-29 18:50:20 +0800

    completed first part of lab1

---
+++ skeleton-sp18-master/lab1/HelloNumbers.java
@@ -0,0 +1,20 @@
+public class HelloNumbers{
+	public static void main(String[] args){
+		int x;
+		x = 0;
+		while(x < 10){
+			System.out.println(x);
+			x = x + 1;
+		}
+	}
+}
+
+/*
+	1. Before Java variables can be used, they must be declared.
+	2. Java variables must have a specific change.
+	3. Java variable types can never change.
+	4. Types are verified before the code even runs!!!
+		* Easy to debug before run
+		* ? Uneasy for users to get a bug
+		* Clarify the types for variables
+*/
